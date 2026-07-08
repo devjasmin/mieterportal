9c1ccf8d8ae24b9a1bb91b9f42e11ad57de5e767
Jasmin <j.longa@gmx.ch>
2026-07-08 17:51:35 +0200

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/mieterportal/",
+  base: "/mieterportal",
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
 });

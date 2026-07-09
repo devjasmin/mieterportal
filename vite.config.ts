09892123da7d2ae3464837f6b872318249108c15
Jasmin <j.longa@gmx.ch>
2026-07-09 10:51:27 +0200

    Remove rolldown plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import babel from "@rolldown/plugin-babel";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/mieterportal",
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [react()],
 });
